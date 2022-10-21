ccc031d6732991bddaae148ed460259a2b410aca
Jean-Paul Ricaud <jean-paul.ricaud@synchrotron-soleil.fr>
2022-10-21 17:19:03 +0200

    Preparing migration to KiCad v7 ...

--- scripts/symbols_to_csv/parameters/parameters.go
+++ scripts/symbols_to_csv/parameters/parameters.go
@@ -5,7 +5,7 @@ const Resistors_0603_file    string = "resistors_0603"
 const Resistors_0805_file    string = "resistors_0805"
 const Resistors_2512_file    string = "resistors_2512"
 const Resistors_network_file string = "resistors_network"
-var Resistors_data = [][]string {{"key", "symbol", "Value", "Footprint", "Datasheet", "Manufacturer", "Manufacturer ref", "Supplier", "Supplier ref", "Qty", "Impedance", "Power", "Precision", "Type", "Height", "Package", "Price", "ki_keywords", "ki_description"}}
+var Resistors_data = [][]string {{"key", "symbol", "Value", "Footprint", "Datasheet", "Manufacturer", "Manufacturer ref", "Supplier", "Supplier ref", "Mounted", "Impedance", "Power", "Precision", "Type", "Height", "Package", "Price", "ki_keywords", "ki_description"}}
 const Resistors_folder string = "resistors"
 
 // Capacitors
@@ -14,7 +14,7 @@ const Capacitors_ceramic_0603_file string = "capacitors_ceramic_0603"
 const Capacitors_ceramic_0805_file string = "capacitors_ceramic_0805"
 const Capacitors_ceramic_1210_file string = "capacitors_ceramic_1210"
 const Capacitors_tantalum_file     string = "capacitors_tantalum"
-var Capacitors_data = [][]string {{"key", "symbol", "Value", "Footprint", "Datasheet", "Manufacturer", "Manufacturer ref", "Supplier", "Supplier ref", "Qty", "Capacitance", "Voltage", "Precision", "Type", "Height", "Package", "Price", "ki_keywords", "ki_description"}}
+var Capacitors_data = [][]string {{"key", "symbol", "Value", "Footprint", "Datasheet", "Manufacturer", "Manufacturer ref", "Supplier", "Supplier ref", "Mounted", "Capacitance", "Voltage", "Precision", "Type", "Height", "Package", "Price", "ki_keywords", "ki_description"}}
 const Capacitors_folder string = "capacitors"
 
 // ADC
@@ -32,6 +32,16 @@ const Amplifier_folder string = "analogs"
 const Comparator_file   string = "Comparator"
 const Comparator_folder string = "analogs"
 
+// Connectors
+const CPCI_connectors     string = "CPCI_connectors"
+const HE_connectors       string = "HE_connectors"
+const Lemo_connectors     string = "Lemo_connectors"
+const PSU_connectors      string = "PSU_connectors"
+const RJ45_connectors     string = "RJ45_connectors"
+const SMA_connectors      string = "SMA_connectors"
+const Terminal_connectors string = "Terminal_connectors"
+const Connectors_folder   string = "connectors"
+
 // IC
-var IC_data = [][]string {{"key", "symbol", "Value", "Footprint", "Datasheet", "Manufacturer", "Manufacturer ref", "Supplier", "Supplier ref", "Qty", "Note 1", "Note 2", "Note 3", "Note 4", "Height", "Package", "Price", "ki_keywords", "ki_description"}}
+var IC_data = [][]string {{"key", "symbol", "Value", "Footprint", "Datasheet", "Manufacturer", "Manufacturer ref", "Supplier", "Supplier ref", "Mounted", "Note 1", "Note 2", "Note 3", "Note 4", "Height", "Package", "Price", "ki_keywords", "ki_description"}}
 
